997a0517c152a88fda9f8da031f6cb2dd2a2055a
Maksim Goryunov <max.gor@list.ru>
2020-07-11 02:19:55 +0300

    week_4 commit

--- avatar.js
+++ avatar.js
@@ -1,61 +1,40 @@
 
 const reviewItems = document.querySelectorAll('.reviews__items');
 const switcherItems = document.querySelectorAll('.reviews__switcher-item');
+const switcherImg = document.querySelectorAll('.interactive-avatar__img');
 
 const reviewArr = Array.from(reviewItems);
 const switcherArr = Array.from(switcherItems);
-
+const switcherImgArr = Array.from(switcherImg);
 
 
 document.querySelector('.reviews__switcher').addEventListener('click', e => {
   e.preventDefault();
+  var elem = e.target;
 
-$this=(e.currentTarget);
-console.log($this);
+  const i = switcherImgArr.indexOf(elem);
 
- switcherArr.forEach((element) =>{
 
-         element.classList.remove('interactive-avatar--active');
-         
+  switcherArr.forEach((element) => {
 
-      });
+    element.classList.remove('interactive-avatar--active');
 
-      const i = switcherArr.indexOf($this);
+  });
+
+  switcherArr[i].classList.add('interactive-avatar--active');
 
   if (i !== -1) {
 
+    reviewArr.forEach((element) => {
+      element.style.display = 'none';
+    });
 
-
-    if (i == 1) {
-      reviewArr[i - 1].style.display = 'none';
-      reviewArr[i].style.display = 'flex';
-      switcherArr[i].classList.add('interactive-avatar--active');
-
-     
-     
-    }
-
-    if (i == 2) {
-      reviewArr[i - 1].style.display = 'none';
-      reviewArr[i - 2].style.display = 'none';
-      reviewArr[i].style.display = 'flex';
-      switcherArr[i].classList.add('interactive-avatar--active');
-
-    
-
-    }
-
-    else {
-      reviewArr[i].style.display = 'flex';
-      switcherArr[i].classList.add('interactive-avatar--active');
-
-     
-
-
-    }
+    reviewArr[i].style.display = 'flex';
   }
 
 });
 
 
-  
+
+
+
